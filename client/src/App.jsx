d4d024a560ca2170dea98e3803615ec909a3c86f
RamenHoang <anhnguyenhoang321@gmail.com>
2026-01-25 22:57:03 +0700

    Remove redundant section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,39 +23,7 @@ function MainLayout() {
       <About />
       <CollectionsSection />
       <BlogSection />
-
-      <Section
-        title="RUBY"
-        description="Ruby (Hồng ngọc) là một trong 4 loại đá quý hiếm nhất thế giới. Sắc đỏ huyết bồ câu đặc trưng tượng trưng cho tình yêu nồng cháy và quyền lực vĩnh cửu."
-        buttonText="XEM BỘ SƯU TẬP"
-        reverse={true}
-        image="https://placehold.co/600x400/111/FFF?text=Ruby"
-      />
-
-      <Section
-        title="SAPPHIRE"
-        description="Sapphire (Lam ngọc) mang sắc xanh biển sâu thẳm, biểu tượng của sự trung thành, niềm tin và hy vọng. Đây là loại đá quý được giới hoàng gia ưa chuộng."
-        buttonText="XEM BỘ SƯU TẬP"
-        image="https://placehold.co/600x400/111/FFF?text=Sapphire"
-      />
-
       <Portfolio />
-
-      <Section
-        title="KIM CƯƠNG"
-        description="Kim cương - Nữ hoàng của các loại đá quý. Với độ cứng tuyệt đối và vẻ đẹp lấp lánh vĩnh cửu, kim cương là biểu tượng của tình yêu bất diệt."
-        buttonText="XEM BỘ SƯU TẬP"
-        reverse={true}
-        image="https://placehold.co/600x400/111/FFF?text=Diamond"
-      />
-
-      <Section
-        title="NGỌC TRAI"
-        description="Ngọc trai mang vẻ đẹp thuần khiết và sang trọng. Mỗi viên ngọc là một kiệt tác của thiên nhiên, tôn vinh vẻ đẹp dịu dàng của người phụ nữ."
-        buttonText="XEM BỘ SƯU TẬP"
-        image="https://placehold.co/600x400/111/FFF?text=Pearl"
-      />
-
       <Frames />
       <Footer />
     </>
